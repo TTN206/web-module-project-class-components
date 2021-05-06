7a5dcd179ef045f9595d409d924ab862d3272224
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-05 21:49:39 -0700

    saving for Lambda Canvas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+
+const todoTasks = [
+  {
+    task: 'Clean Dishes',
+    id: 1234,
+    completed: false
+  },
+  {
+    task: 'Sweep and Mop',
+    id: 2345,
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state = {
+      todoTasks: todoTasks,
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
+import React from 'react';
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+class TodoList extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h3>TodoList is here!</h3>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
